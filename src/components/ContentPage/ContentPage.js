43b3172e67adcc86a5bc63c89ee31b5168b3e208
dethstrobe <dethstrobe@gmail.com>
2015-10-07 23:00:39 -0600

    Starting Omae, turning spaces in to tabs
    Probably broke everything

--- src/components/ContentPage/ContentPage.js
+++ src/components/ContentPage/ContentPage.js
@@ -7,29 +7,29 @@ import withStyles from '../../decorators/withStyles';
 @withStyles(styles)
 class ContentPage extends Component {
 
-  static propTypes = {
-    path: PropTypes.string.isRequired,
-    content: PropTypes.string.isRequired,
-    title: PropTypes.string,
-  };
+	static propTypes = {
+		path: PropTypes.string.isRequired,
+		content: PropTypes.string.isRequired,
+		title: PropTypes.string,
+	};
 
-  static contextTypes = {
-    onSetTitle: PropTypes.func.isRequired,
-  };
+	static contextTypes = {
+		onSetTitle: PropTypes.func.isRequired,
+	};
 
-  render() {
-    this.context.onSetTitle(this.props.title);
-    return (
-      <div className="ContentPage">
-        <div className="ContentPage-container">
-          {
-            this.props.path === '/' ? null : <h1>{this.props.title}</h1>
-          }
-          <div dangerouslySetInnerHTML={{__html: this.props.content || ''}} />
-        </div>
-      </div>
-    );
-  }
+	render() {
+		this.context.onSetTitle(this.props.title);
+		return (
+			<div className="ContentPage">
+				<div className="ContentPage-container">
+					{
+						this.props.path === '/' ? null : <h1>{this.props.title}</h1>
+					}
+					<div dangerouslySetInnerHTML={{__html: this.props.content || ''}} />
+				</div>
+			</div>
+		);
+	}
 
 }
 
